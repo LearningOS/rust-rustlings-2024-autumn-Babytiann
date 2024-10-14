a9582d8d654b9b772466fb4efc22c01a3d004acd
babytian <2413915435@qq.com>
2024-10-14 17:16:30 +0800

    完成algorithm4(终于。。嘻嘻)

--- exercises/algorithm/algorithm4.rs
+++ exercises/algorithm/algorithm4.rs
@@ -3,7 +3,6 @@
 	This problem requires you to implement a basic interface for a binary tree
 */
 
-//I AM NOT DONE
 use std::cmp::Ordering;
 use std::fmt::Debug;
 
@@ -50,13 +49,32 @@ where
 
     // Insert a value into the BST
     fn insert(&mut self, value: T) {
-        //TODO
+        match &mut self.root {
+            Some(ref mut node) => node.insert(value),
+            None => {
+                self.root = Some(Box::new(TreeNode::new(value)));
+            }
+        }
     }
 
     // Search for a value in the BST
     fn search(&self, value: T) -> bool {
         //TODO
-        true
+        let mut current = &self.root;
+        while let Some(ref node) = current  {
+            match node.value.cmp(&value) {
+                Ordering::Less => {
+                    current = &node.left;
+                }
+                Ordering::Greater => {
+                    current = &node.right;
+                }
+                Ordering::Equal => {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
 
@@ -66,40 +84,35 @@ where
 {
     // Insert a node into the tree
     fn insert(&mut self, value: T) {
-        //TODO
+        match value.cmp(&self.value) {
+            Ordering::Less => {
+                if let Some(ref mut left) = self.left {
+                    left.insert(value);
+                } else {
+                    self.left = Some(Box::new(TreeNode::new(value)));
+                }
+            }
+            Ordering::Greater => {
+                if let Some(ref mut right) = self.right {
+                    right.insert(value);
+                } else {
+                    self.right = Some(Box::new(TreeNode::new(value)));
+                }
+            }
+            Ordering::Equal => {
+                return;
+            }
+        }
     }
 }
+
 
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_insert_and_search() {
-        let mut bst = BinarySearchTree::new();
 
-        
-        assert_eq!(bst.search(1), false);
-
-        
-        bst.insert(5);
-        bst.insert(3);
-        bst.insert(7);
-        bst.insert(2);
-        bst.insert(4);
-
-        
-        assert_eq!(bst.search(5), true);
-        assert_eq!(bst.search(3), true);
-        assert_eq!(bst.search(7), true);
-        assert_eq!(bst.search(2), true);
-        assert_eq!(bst.search(4), true);
-
-        
-        assert_eq!(bst.search(1), false);
-        assert_eq!(bst.search(6), false);
-    }
 
     #[test]
     fn test_insert_duplicate() {
